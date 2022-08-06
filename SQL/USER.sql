59510ce75df9bc38654d89f342adceecd8785f88
SaffatZabin-17 <75508086+SaffatZabin-17@users.noreply.github.com>
2022-08-06 22:05:02 +0600

    Add files via upload

---
+++ SQL/USER.sql
@@ -0,0 +1,13 @@
+create table "USER"
+(
+    USER_ID  NUMBER       not null
+        constraint USER_PK
+            primary key,
+    NAME     VARCHAR2(30) not null,
+    EMAIL    VARCHAR2(30),
+    PASSWORD VARCHAR2(30),
+    IMAGE    VARCHAR2(100)
+)
+/
+
+
