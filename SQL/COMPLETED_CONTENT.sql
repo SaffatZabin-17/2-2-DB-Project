091370eff2362b5bc6dfbf59c2f381cc49665c97
SaffatZabin-17 <75508086+SaffatZabin-17@users.noreply.github.com>
2022-08-07 18:06:49 +0600

    Update COMPLETED_CONTENT.sql

--- SQL/COMPLETED_CONTENT.sql
+++ SQL/COMPLETED_CONTENT.sql
@@ -1,1 +1,13 @@
-
+create table COMPLETED_CONTENT
+(
+    USER_ID        NUMBER not null
+        constraint COMPLETED_CONTENT_USER_USER_ID_FK
+            references "USER",
+    CONTENT_ID     NUMBER not null
+        constraint COMPLETED_CONTENT_CONTENTS_CONTENT_ID_FK
+            references CONTENTS,
+    OBTAINED_MARKS NUMBER not null,
+    constraint COMPLETED_CONTENT_PK
+        primary key (USER_ID, CONTENT_ID)
+)
+/
