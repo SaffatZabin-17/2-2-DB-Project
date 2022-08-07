0c2a4980502f778bff8d23d3ec2de94e3010f6c7
SaffatZabin-17 <75508086+SaffatZabin-17@users.noreply.github.com>
2022-08-07 18:58:02 +0600

    Update INSERT_INTO_USER.sql

--- SQL/INSERT_INTO_USER.sql
+++ SQL/INSERT_INTO_USER.sql
@@ -1,3 +1,4 @@
+INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00001, 'Saffat Zabin', 'saffatzabin08430843@gmail.com', 'password1', 'C://Users/Downloads/Image01.png');
 INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00002, 'Riad Ahmed Anonto', 'anonto69420@gmail.com', 'password2', 'C://Users/Downloads/Image02.png');
 INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00003, 'Farhan Tahmid Ryan', 'farhanitrate35@gmail.com', 'password3', 'C://Users/Downloads/Image03.png');
 INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00004, 'Hafijul Haque Chowdhury', 'nabid.hasan@gmail.com', 'password4', 'C://Users/Downloads/Image04.png');
@@ -6,5 +7,9 @@ INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00006, 'Md Mo
 INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00007, 'Md. Lal Mia', 'lal.mia@gmail.com', 'password7', 'C://Users/Downloads/Image07.png');
 INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00008, 'Asif Azad', 'asif.azad@gmail.com', 'password8', 'C://Users/Downloads/Image08.png');
 INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00009, 'Asif Al Shahriar', 'asif.shahriar@gmail.com', 'password9', 'C://Users/Downloads/Image09.png');
-INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (000010, 'Shattik Islam', 'shattik.islam@gmail.com', 'password10', 'C://Users/Downloads/Image10.png');
+INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00010, 'Shattik Islam', 'shattik.islam@gmail.com', 'password10', 'C://Users/Downloads/Image10.png');
+INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00011, 'Ibnul Islam', 'ibnul.islam@gmail.com', 'password11', 'C://Users/Downloads/Image11.png');
+INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00012, 'Ishtiak Ahmed', 'ishtiak.ahmed@gmail.com', 'password12', 'C://Users/Downloads/Image12.png');
+INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00013, 'Abrar Faiyaj', 'abrar.faiyaz@gmail.com', 'password13', 'C://Users/Downloads/Image13.png');
+INSERT INTO "USER" (USER_ID, NAME, EMAIL, PASSWORD, IMAGE) VALUES (00014, 'Asibur Rahman', 'asib789@gmail.com', 'password14', 'C://Users/Downloads/Image14.png');
 
