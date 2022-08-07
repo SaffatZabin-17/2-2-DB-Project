2935823eaa04f8f87d0849008c02a7fd79df9798
SaffatZabin-17 <75508086+SaffatZabin-17@users.noreply.github.com>
2022-08-07 17:43:28 +0600

    Update COMMENT.sql

--- SQL/COMMENT.sql
+++ SQL/COMMENT.sql
@@ -1,1 +1,11 @@
-
+create table "COMMENT"
+(
+    CONTENT_ID  NUMBER         not null
+        constraint COMMENT_CONTENTS_CONTENT_ID_FK
+            references CONTENTS,
+    USER_ID     NUMBER         not null
+        constraint COMMENT_USER_USER_ID_FK
+            references "USER",
+    DESCRIPTION VARCHAR2(4000) not null
+)
+/
