3b519f8d8cac95b7a9eb6c2ec55a123ada0c72cf
SaffatZabin-17 <75508086+SaffatZabin-17@users.noreply.github.com>
2022-08-07 18:41:28 +0600

    Update WRITTEN_DOCUMENT.sql

--- SQL/WRITTEN_DOCUMENT.sql
+++ SQL/WRITTEN_DOCUMENT.sql
@@ -1,1 +1,12 @@
+create table WRITTEN_DOCUMENT
+(
+    CONTENT_ID  NUMBER         not null
+        constraint WRITTEN_DOCUMENT_PK
+            primary key
+        constraint WRITTEN_DOCUMENT_CONTENTS_CONTENT_ID_FK
+            references CONTENTS,
+    DESCRIPTION VARCHAR2(4000) not null
+)
+/
 
+INSERT INTO WRITTEN_DOCUMENT (CONTENT_ID, DESCRIPTION) VALUES (1, 'Written Documents go here');
