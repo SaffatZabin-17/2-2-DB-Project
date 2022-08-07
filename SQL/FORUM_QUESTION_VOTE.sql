090e0c216defd8077c6f372c68727c5b6ce67607
SaffatZabin-17 <75508086+SaffatZabin-17@users.noreply.github.com>
2022-08-07 18:30:58 +0600

    Update FORUM_QUESTION_VOTE.sql

--- SQL/FORUM_QUESTION_VOTE.sql
+++ SQL/FORUM_QUESTION_VOTE.sql
@@ -1,1 +1,14 @@
+create table FORUM_QUESTION_VOTE
+(
+    FORUM_QUESTION_ID NUMBER not null
+        constraint FORUM_QUESTION_VOTE_FORUM_QUESTION_FORUM_QID_FK
+            references FORUM_QUESTION,
+    USER_ID           NUMBER not null
+        constraint FORUM_QUESTION_VOTE_USER_USER_ID_FK
+            references "USER",
+    constraint FORUM_QUESTION_VOTE_PK
+        primary key (FORUM_QUESTION_ID, USER_ID)
+)
+/
 
+INSERT INTO FORUM_QUESTION_VOTE (FORUM_QUESTION_ID, USER_ID) VALUES (1, 11);
