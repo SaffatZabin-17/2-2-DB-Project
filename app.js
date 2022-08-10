42f4faa7771ff0c9afacd80382f2bd23c7bd2d9a
Anonto050 <65712214+Anonto050@users.noreply.github.com>
2022-08-10 09:15:55 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,87 @@
-/*
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+async = require('async');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const mongoose = require("mongoose");
+const oracleDB = require('oracledb');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});         
+
+app.get("/", (req, res) => {
+  res.redirect("index");
+});
+
+
+//middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
+
+//Express-session
+app.use(session({
+   secret: 'secret',
+   saveUninitialized: true,
+   resave: true
+}));
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Express validator
+app.use(expressValidator({
+  errorFormatter: function(param,msg,value){
+    var namespace = param.split('.'),
+    root = namespace.shift(),
+    formParam = root;
+
+    while(namespace.length){
+      formParam += '[' + namespace.shift() + ']'; 
+    }
+    return{
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+//connect flash
+app.use(flash());
+
+//Global vars
+app.use(function(req,res,next){
+  res.locals.messages = require('express-messages')(req,res);
+  next();
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,36 +99,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, function () {
+console.log("Express server listening on port 3000");
+});
+
+
 module.exports = app;
-*/
-
-
-const express = require('express');
-const app = express();
-const models = require('./models/students');
-const router = express.Router();
-const database = require('./models/databaseConnect');
-const model = require('./models/course');
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, async () => {
-    try {
-        await database.startup()
-        console.log(`Server started on ${PORT}`)
-    } catch (error) {
-        console.log('Error starting the database')
-        process.exit(1)
-    }
-})
-
-let result = models.getUserByID(11);
-result.then(function (result) {
-    console.log(result);
-})
-
-result = model.getAllCourse();
-result.then(function (result){
-    console.log(result);
-})
-//models.insertAccountIntoDB(15, 'Afif Hasan', 'afif.hasan@gmail.com', 'password15', 'C://User/Downloads/Imgae15.png');
